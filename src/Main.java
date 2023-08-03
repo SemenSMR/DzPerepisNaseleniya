ffdf3a554d7ed54e27584f1e34d22236e42cf029
Evgeniy <svorovali@mail.ru>
2023-08-03 11:41:42 +0300

    file init

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+            long ageCount = persons.stream()
+                    .filter(person -> person.getAge() < 18)
+                    .count();
+
+            System.out.println("Количество несовершеннолетних: " + ageCount);
+            List<String> Families = persons.stream()
+                    .filter(person -> person.getSex() == Sex.MAN && person.getAge() >= 18 && person.getAge() <= 27)
+                    .map(Person::getFamily)
+                    .collect(Collectors.toList());
+
+            System.out.println("Фамилии призывников: " + Families);
+            List<Person> potentialEmployers = persons.stream()
+                    .filter(person -> person.getEducation() == Education.HIGHER
+                            && ((person.getSex() == Sex.WOMAN && person.getAge() >= 18 && person.getAge() <= 60)
+                            || (person.getSex() == Sex.MAN && person.getAge() >= 18 && person.getAge() <= 65)))
+                    .sorted(Comparator.comparing(Person::getFamily))
+                    .collect(Collectors.toList());
+
+            System.out.println("Потенциально работоспособные люди с высшим образованием:");
+            potentialEmployers.forEach(System.out::println);
+
+        }
+    }
+}
